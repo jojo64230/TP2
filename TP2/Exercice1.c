e17f32c8f850025c2f833b8c5ff4cdc205ab5fb5
jojo64230 <jolan.thierry@ens-rennes.fr>
2024-01-19 12:14:39 +0100

    fourth commit

---
+++ TP2/Exercice1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int pascal(int i,int j){
+      for (int i=1; i==n; i++){
+        for (int j=1; j== ;j++){
+            
+        }
+    }
+    return m;
+}
+
+
+int p(int i, int j){
+    if (n == 0){
+        return 1;
+    }
+    else{    
+        for (int i=0; (i+1)<n; i++){
+            p(i,j)=p(i-1,j)+p(i-1,j-1);
+            k=p(i-j)
+    }
+    }
+    return k; 
+    
+}
